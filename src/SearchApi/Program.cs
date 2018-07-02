cd5748622b9fe3e9007b678633377186867a4870
SURASAKK <surasak.bcircle@gmail.com>
2018-07-02 19:25:14 +0700

    add Serilog

---
+++ src/SearchApi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace SearchApi
+{
+    
+
+
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+          .WriteTo.RollingFile("log-{Date}.txt")
+            .CreateLogger();
+    Log.Information("This will be written to the rolling file set");
+            Log.Information("Hello, world!");
+
+            CreateWebHostBuilder(args).Build().Run();
+
+           
+            
+
+            
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+ 
+}
